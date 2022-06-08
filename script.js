29cea494e7b10072cfb36b14aaf7d81400991727
DWKanas <dkanas0122@gmail.com>
2022-06-07 21:45:13 -0700

    Added code to save to local storage

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ console.log(currentHour)
 
 currentDayEl.text(moment().format('LLL'));
 
+// containerEl.on('click', 'button', )
 
 function makeCalender() {
    for (var i = 9; i < 18; i ++) {
@@ -19,17 +20,28 @@ function makeCalender() {
     var inputTextAreaEl = $('<textarea>')
     var saveButtonEl = $('<button>')
     saveButtonEl.addClass('saveBtn')
+    saveButtonEl.attr('id', '${i}')
 
     containerEl.append(timeBlockEl)
     timeBlockEl.append(hourContainerEl)
     timeBlockEl.append(rowEl)
     rowEl.append(inputTextAreaEl)
     rowEl.append(saveButtonEl)
-    
+
     console.log('test')
 
    }
 
 }
 
+function keepText() {
+    for (var i = 9; i < 18; i ++) {
+        if($(this).attr('id') === i) {
+            localStorage.setItem('todo${i}', $(this).siblings().val())
+        }
+    }
+}
+
+
+// function 
 makeCalender()
